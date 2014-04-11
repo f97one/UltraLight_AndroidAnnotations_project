aeb1789af4c5861b1897f19c4e8d06eacc492bab
HAJIME Fukuna <f97one@gmail.com>
2014-04-12 00:44:26 +0900

    超簡単なAndroidAnnotations適用プロジェクトを作成。

---
+++ app/src/main/java/net/formula97/aaex/app/MainActivity.java
@@ -0,0 +1,54 @@
+package net.formula97.aaex.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity
+public class MainActivity extends Activity {
+
+	@ViewById
+	TextView textView;
+	@ViewById
+	Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+	@Click(R.id.button)
+	public void button_Click() {
+		textView.setText("AndroidAnnotations available!");
+	}
+
+}
