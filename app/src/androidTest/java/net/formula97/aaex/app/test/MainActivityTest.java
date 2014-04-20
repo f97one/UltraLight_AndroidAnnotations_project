a33db56db478394e90d48af9efb7631709188777
HAJIME Fukuna <f97one@gmail.com>
2014-04-20 21:21:14 +0900

    テストコードの雛形を生成。

---
+++ app/src/androidTest/java/net/formula97/aaex/app/test/MainActivityTest.java
@@ -0,0 +1,21 @@
+package net.formula97.aaex.app.test;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by f97one on 14/04/20.
+ */
+public class MainActivityTest extends TestCase {
+	public void setUp() throws Exception {
+		super.setUp();
+
+	}
+
+	public void tearDown() throws Exception {
+
+	}
+
+	public void testButton_Click() throws Exception {
+
+	}
+}
